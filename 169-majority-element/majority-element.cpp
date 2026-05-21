4d90ae08938ab8d73ead2f3e801acf9a4605a30f
Rajabu Nisha <rajabunisha1907@gmail.com>
2026-05-21 17:14:48 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (27.09%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        int n=nums.size();
         int cnt=1;
         int element=nums[0];
         for(int i=1;i<nums.size();i++)
@@ -12,6 +13,14 @@ public:
             }
             else cnt--;
         }
-        return element;
+        //just to check whether the element is really major element for the given condition
+        int cnt1=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==element) cnt1++;
+        }
+        if(cnt1>n/2)  return element;
+        return -1;
+        
     }
 };
