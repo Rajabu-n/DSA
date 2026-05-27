c524b3aac63fff36e4d971c2c295666c0ac26cc2
Rajabu Nisha <rajabunisha1907@gmail.com>
2026-05-27 20:14:29 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (6.99%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -5,9 +5,14 @@ public:
         int n=matrix[0].size();
         for(int i=0;i<m;i++)
         {
-            for(int j=0;j<n;j++)
+            int low=0;
+            int high=n-1;
+            while(low<=high)
             {
-                if(matrix[i][j]==target)return true;
+                int mid=(low+high)/2;
+                if(matrix[i][mid]==target)return true;
+                else if(matrix[i][mid]>target) high=mid-1;
+                else low=mid+1;
             }
         }
         return false;
