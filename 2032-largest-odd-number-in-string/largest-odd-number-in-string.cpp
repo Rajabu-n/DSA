2d20623e4c9e865e08a558cd9f91aa0300a3fda5
Rajabu Nisha <rajabunisha1907@gmail.com>
2026-05-31 21:54:05 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (65.63%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -3,7 +3,7 @@ public:
     string largestOddNumber(string num) {
             for(int i=num.length();i>=0;i--)
             {
-                if((num[i]-'0')%2==1)
+                if((num[i]-'0')%2!=0)
                 {
                     return num.substr(0,i+1);
                 }
