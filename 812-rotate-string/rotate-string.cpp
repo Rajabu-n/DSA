ba78d29b782fdee90890d4a308d5da13d648c509
Rajabu Nisha <rajabunisha1907@gmail.com>
2026-06-01 18:10:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (12.96%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -3,15 +3,11 @@ public:
     bool rotateString(string s, string goal) {
         int n=s.size();
         if(s.size()!=goal.size())return false;
-        for(int i=0;i<s.size();i++)
+        string doubled=s+s;
+        for(int i=0;i<2*n;i++)
         {
-            char word=s[0];
-            for(int j=1;j<s.size();j++)
-            {
-                s[j-1]=s[j];
-            }
-            s[n-1]=word;
-            if(s==goal)return true;
+            string rotated=doubled.substr(i,n);
+            if(rotated==goal)return true;
         }
         return false;
     }
