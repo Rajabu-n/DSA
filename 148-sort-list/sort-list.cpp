9284529e2c1fb0ac81c20c1005dc318ff01b92d2
Rajabu Nisha <rajabunisha1907@gmail.com>
2026-06-07 15:47:12 +0530

    Time: 55 ms (15.42%) | Memory: 75.7 MB (31.28%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -28,18 +28,9 @@ public:
             }
             temp=temp->next;
         }
-            while(list1!=nullptr)
-            {
-                temp->next=list1;
-                temp=temp->next;
-                list1=list1->next;
-            }
-            while(list2!=nullptr)
-            {
-                temp->next=list2;
-                list2=list2->next;
-                temp=temp->next;
-            }
+            if(list1)  temp->next=list1;  
+            if(list2!=nullptr)  temp->next=list2;
+               
         return dummy->next;
         
     }
