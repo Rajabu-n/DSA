371d028475d69c7ad5db7afdeaf34ccdbebfc725
Rajabu Nisha <rajabunisha1907@gmail.com>
2026-06-07 17:43:17 +0530

    Time: 59 ms (41.53%) | Memory: 23.9 MB (84.99%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,20 +8,48 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_set<ListNode*>freq;
-        ListNode* temp=headA;
-        while(temp)
+    int getDifference(ListNode *headA,ListNode *headB)
+    {
+        int len1=0;
+        int len2=0;
+        ListNode* temp1=headA;
+        ListNode* temp2=headB;
+        while(temp1)
         {
-            freq.insert(temp);
-            temp=temp->next;
+            len1++;
+            temp1=temp1->next;
         }
-        
-        ListNode* i=headB;
-        while(i)
+        while(temp2)
         {
-            if(freq.find(i)!=freq.end())return i;
-            i=i->next;
+            len2++;
+            temp2=temp2->next;
+        }
+        return len1-len2;
+    }
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int dif=getDifference(headA,headB);
+
+        if(dif>0)
+        {
+            while(dif!=0)
+            {
+                headA=headA->next;
+                dif--;
+            }
+        }
+        else
+        {
+            while(dif!=0)
+            {
+                headB=headB->next;
+                dif++;
+            }
+        }
+        while(headA)
+        {
+            if(headA==headB) return headA;
+            headA=headA->next;
+            headB=headB->next;
         }
         return nullptr;
     }
