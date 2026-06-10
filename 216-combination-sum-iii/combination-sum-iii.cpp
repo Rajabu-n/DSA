6310aba9a2df2fa9993f6cb115863e9804cb9b40
Rajabu Nisha <rajabunisha1907@gmail.com>
2026-06-10 14:41:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (30.89%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    void findCombination(int ind,vector<int>& arr,vector<vector<int>>& ans,vector<int>&ds,int k,int target)
+    void findCombination(int i,vector<vector<int>>& ans,vector<int>&ds,int k,int target)
     {
-        if(ind==arr.size()){
+        if(i==10){
             if(target==0 && ds.size()==k)
             {
                 ans.push_back(ds);
             }
             return;
         }
-        ds.push_back(arr[ind]);
-        findCombination(ind+1,arr,ans,ds,k,target-arr[ind]);
+        ds.push_back(i);
+        findCombination(i+1,ans,ds,k,target-i);
         ds.pop_back();
-        findCombination(ind+1,arr,ans,ds,k,target);
+        findCombination(i+1,ans,ds,k,target);
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> ans;
         vector<int>ds;
-        vector<int>arr;
-        for(int i=1;i<=9;i++)
-        {
-            arr.push_back(i);
-        }
-        findCombination(0,arr,ans,ds,k,n);
+        findCombination(1,ans,ds,k,n);
         return ans;
     }
 };
