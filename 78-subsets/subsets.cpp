7acc67fe4c490d9523884d6e3730c11d7ca39b77
Rajabu Nisha <rajabunisha1907@gmail.com>
2026-06-13 22:32:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (81.06%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    void findSubset(int ind,vector<int>& nums,vector<vector<int>>& ans,vector<int>& ds)
-    {
-        if(ind==nums.size())
-        {
-            ans.push_back(ds);
-            return;
-        }
-        ds.push_back(nums[ind]);
-        findSubset(ind+1,nums,ans,ds);
-        ds.pop_back();
-        findSubset(ind+1,nums,ans,ds);
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        findSubset(0,nums,ans,ds);
+        int n=nums.size();
+        int subsets=(1<<n); //2^n
+        for(int num=0;num<subsets;num++)
+        {
+            vector<int>ds;
+            for(int i=0;i<n;i++)
+            {
+                if(num&(1<<i)) ds.push_back(nums[i]);
+            }
+            ans.push_back(ds);
+        }
         return ans;
     }
 };
