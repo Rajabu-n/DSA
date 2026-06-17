67cc9594ab562eca33e8cc7bfc6dc73b531a69cd
Rajabu Nisha <rajabunisha1907@gmail.com>
2026-06-17 19:23:09 +0530

    Time: 32 ms (22.25%) | Memory: 87.9 MB (12.33%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,54 +1,48 @@
 class Solution {
 public:
-    vector<int>findnse(vector<int> & heights)
+    vector<int>findpse(vector<int>& heights,int n)
     {
-        stack<int> st;
-        int n=heights.size();
-         vector<int>nse(n);
+        vector<int>pse(n);
+        stack<int>st;
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty() && heights[st.top()]>heights[i])
+            {
+               st.pop();
+            }
+            pse[i]= st.empty()?-1 : st.top();
+            st.push(i);
+        }
+        return pse;
+    }
+    vector<int>findnse(vector<int>& heights,int n)
+    {
+        vector<int>nse(n);
+        stack<int>st;
         for(int i=n-1;i>=0;i--)
         {
             while(!st.empty() && heights[st.top()]>=heights[i])
             {
-                st.pop();
+               st.pop();
             }
-            nse[i]=st.empty() ? n :st.top();
+            nse[i]= st.empty()?n : st.top();
             st.push(i);
-           
-
         }
         return nse;
-
     }
-    vector<int>findpse(vector<int> & heights)
-    {
-         stack<int> st;
-         int n=heights.size();
-        vector<int>pse(n);
-         for(int i=0;i<n;i++)
-         {
-            while(!st.empty() && heights[st.top()]>=heights[i])
-            {
-                st.pop();
-            }
-            pse[i]=st.empty() ? -1: st.top(); 
-            st.push(i);
-            
-         }
-         return pse;
-
-    }
-
     int largestRectangleArea(vector<int>& heights) {
-        int sum=INT_MIN;
+        int sum=0;
+        int maxval=0;
         int n=heights.size();
-        vector<int>nse=findnse(heights);
-        vector<int>pse=findpse(heights);
+        vector<int>pse=findpse(heights,n);
+        vector<int>nse=findnse(heights,n);
         for(int i=0;i<n;i++)
         {
-            int width=nse[i]-pse[i]-1;
-            int val=heights[i]*width;
-            sum=max(sum,val);
+                int left=i-pse[i]-1;
+                int right=nse[i]-i;
+                sum=heights[i]*(left+right);
+                if(sum>maxval) maxval=sum;
         }
-        return sum;
+        return maxval;
     }
 };
