c99cafb3110c0c59f5ae48a605fc887852fb037d
Rajabu Nisha <rajabunisha1907@gmail.com>
2026-06-17 20:27:06 +0530

    Time: 23 ms (53.71%) | Memory: 81.4 MB (71.85%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,48 +1,32 @@
 class Solution {
 public:
-    vector<int>findpse(vector<int>& heights,int n)
-    {
-        vector<int>pse(n);
+    int largestRectangleArea(vector<int>& heights) {
+        int n=heights.size();
         stack<int>st;
+        int maxarea=INT_MIN;
         for(int i=0;i<n;i++)
         {
             while(!st.empty() && heights[st.top()]>heights[i])
             {
-               st.pop();
+                int element=heights[st.top()];
+                st.pop();
+                int nse=i;
+                int pse=st.empty()?-1:st.top();
+                int val=element*(nse-pse-1);
+                maxarea=max(maxarea,val);
             }
-            pse[i]= st.empty()?-1 : st.top();
             st.push(i);
         }
-        return pse;
-    }
-    vector<int>findnse(vector<int>& heights,int n)
-    {
-        vector<int>nse(n);
-        stack<int>st;
-        for(int i=n-1;i>=0;i--)
+        while(!st.empty())
         {
-            while(!st.empty() && heights[st.top()]>=heights[i])
-            {
-               st.pop();
-            }
-            nse[i]= st.empty()?n : st.top();
-            st.push(i);
+            int element=heights[st.top()];
+             st.pop();
+            int pse=st.empty()?-1:st.top();
+            int nse=n;
+            int val=element*(nse-pse-1);
+             maxarea=max(maxarea,val);
+            
         }
-        return nse;
-    }
-    int largestRectangleArea(vector<int>& heights) {
-        int sum=0;
-        int maxval=0;
-        int n=heights.size();
-        vector<int>pse=findpse(heights,n);
-        vector<int>nse=findnse(heights,n);
-        for(int i=0;i<n;i++)
-        {
-                int left=i-pse[i]-1;
-                int right=nse[i]-i;
-                sum=heights[i]*(left+right);
-                if(sum>maxval) maxval=sum;
-        }
-        return maxval;
+        return maxarea;
     }
 };
