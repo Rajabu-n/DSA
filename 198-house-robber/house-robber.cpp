541c3c33bb2a8b22eb9d90833c5c11d6f3e02ab3
Rajabu Nisha <rajabunisha1907@gmail.com>
2026-07-04 15:00:39 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (97.53%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    /*
-    int findmax(vector<int>& nums,int ind,vector<int>& dp)
-    {
-        if(ind==0) return nums[0];
-        if(ind<0) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        int pick=nums[ind]+findmax(nums,ind-2,dp);
-        int nonpick=0+findmax(nums,ind-1,dp);
-        return dp[ind]=max(pick,nonpick);
-    }
-    */
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,-1);
-        dp[0]=nums[0];
-        int pick=0;
-        int notpick=0;
+        if(n==1) return nums[0];
+        int prev2=0;
+        int prev1=nums[0];
         for(int i=1;i<n;i++)
         {
-            pick=nums[i];
-            if(i>1)pick=nums[i]+dp[i-2];
-            notpick=dp[i-1];
-            dp[i]=max(pick,notpick);
+            int pick=nums[i];
+            if(i>1) pick+=prev2;
+            int notpick=prev1;
+
+            int cur=max(pick,notpick);
+            prev2=prev1;
+            prev1=cur;
         }
-        return dp[n-1];
+        return prev1;
     }
 };
