f01146bd9f0bc14b18cb6f2bf9a22c8fc18222ab
Rajabu Nisha <rajabunisha1907@gmail.com>
2026-07-05 16:44:08 +0530

    Time: 35 ms (40.84%) | Memory: 36.3 MB (65.96%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int findway(int ind,vector<int>&coins,int target,vector<vector<int>>&dp)
     {
         if(ind==0)
@@ -15,10 +16,29 @@ public:
         }
         return dp[ind][target]=taken+nottaken;
     }
+    */
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-         return findway(n-1,coins,amount,dp);
-       
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        for(int i=0;i<=amount;i++)
+        {
+            if(i%coins[0]==0) dp[0][i]=1;
+        }
+        for(int ind=1;ind<n;ind++)
+        {
+            for(int t=0;t<=amount;t++)
+            {
+                long long nottaken=dp[ind-1][t];
+                long long taken=0;
+                if(coins[ind]<=t)
+                {
+                    taken=dp[ind][t-coins[ind]];
+                }
+                dp[ind][t]=taken+nottaken;
+            }
+           
+        }
+        
+       return dp[n-1][amount];
     }
 };
