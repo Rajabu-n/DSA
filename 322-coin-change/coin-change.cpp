b153c3c13dba7c8331af3f272fc8027c61b48c92
Rajabu Nisha <rajabunisha1907@gmail.com>
2026-07-05 21:03:16 +0530

    Time: 45 ms (37.40%) | Memory: 49.4 MB (25.04%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, amount + 1); 
-        dp[0] = 0;
-
-        for (int i = 1; i <= amount; i++) {
-            for (int coin : coins) {
-                if (coin <= i) {
-                    dp[i] = min(dp[i], dp[i - coin] + 1);
-                }
+    int findways(vector<int>& coins, int target,int ind,vector<vector<int>>& dp)
+    {
+            if(ind==0)
+            {
+                if(target%coins[ind]==0) return target/coins[ind];
+                return 1e9;
             }
-        }
-        return dp[amount] > amount ? -1 : dp[amount];
+            if(dp[ind][target]!=-1) return dp[ind][target];
+            long long nottake=0+findways(coins,target,ind-1,dp);
+            long long take=INT_MAX;
+            if(coins[ind]<=target)
+            {
+                take=1+findways(coins,target-coins[ind],ind,dp);
+            }
+            return dp[ind][target]= min(take,nottake);
+            
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int ans=findways(coins,amount,n-1,dp);
+        if(ans>=1e9) return -1;
+        return ans;
     }
 };
