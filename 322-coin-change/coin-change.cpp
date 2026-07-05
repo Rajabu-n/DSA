533353f8adff86dc3e1055866bbac16c6529122a
Rajabu Nisha <rajabunisha1907@gmail.com>
2026-07-05 21:30:35 +0530

    Time: 53 ms (19.77%) | Memory: 49 MB (32.55%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int findways(vector<int>& coins, int target,int ind,vector<vector<int>>& dp)
     {
             if(ind==0)
@@ -17,11 +18,31 @@ public:
             return dp[ind][target]= min(take,nottake);
             
     }
+    */
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans=findways(coins,amount,n-1,dp);
-        if(ans>=1e9) return -1;
-        return ans;
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        
+        for(int t=0;t<=amount;t++)
+        {
+            if(t%coins[0]==0)dp[0][t]=t/coins[0];
+            else dp[0][t]=1e9;
+        }
+        for(int ind=1;ind<n;ind++)
+        {
+            for(int t=0;t<=amount;t++)
+            {
+                long long nottake=0+dp[ind-1][t];
+                long long take=INT_MAX;
+                if(coins[ind]<=t)
+                {
+                    take=1+dp[ind][t-coins[ind]];
+                }
+                dp[ind][t]=min(take,nottake);
+            }
+
+        }
+        if(dp[n-1][amount]>=1e9) return -1;
+        else return dp[n-1][amount];
     }
 };
