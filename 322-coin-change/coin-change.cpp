49c2d8593d72c247c11885b6211b86c21bfc26d1
Rajabu Nisha <rajabunisha1907@gmail.com>
2026-09-15 22:05:31 +0530

    Time: 50 ms (27.42%) | Memory: 48.9 MB (41.90%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,48 +1,23 @@
 class Solution {
 public:
-    /*
-    int findways(vector<int>& coins, int target,int ind,vector<vector<int>>& dp)
+    int findsum(vector<int>& coins, int sum,int ind,vector<vector<int>>& dp)
     {
-            if(ind==0)
-            {
-                if(target%coins[ind]==0) return target/coins[ind];
-                return 1e9;
-            }
-            if(dp[ind][target]!=-1) return dp[ind][target];
-            long long nottake=0+findways(coins,target,ind-1,dp);
-            long long take=INT_MAX;
-            if(coins[ind]<=target)
-            {
-                take=1+findways(coins,target-coins[ind],ind,dp);
-            }
-            return dp[ind][target]= min(take,nottake);
-            
+        if(ind==0)
+        {
+            if(sum%coins[0]==0) return sum/coins[0];
+            return 1e9;
+        }
+        if(dp[ind][sum]!=-1) return dp[ind][sum];
+        int pick=1e9;
+        if(coins[ind]<=sum)pick=1+findsum(coins,sum-coins[ind],ind,dp);
+        int notpick=0+findsum(coins,sum,ind-1,dp);
+        return dp[ind][sum]=min(pick,notpick);
     }
-    */
     int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,0));
-        
-        for(int t=0;t<=amount;t++)
-        {
-            if(t%coins[0]==0)dp[0][t]=t/coins[0];
-            else dp[0][t]=1e9;
-        }
-        for(int ind=1;ind<n;ind++)
-        {
-            for(int t=0;t<=amount;t++)
-            {
-                long long nottake=0+dp[ind-1][t];
-                long long take=INT_MAX;
-                if(coins[ind]<=t)
-                {
-                    take=1+dp[ind][t-coins[ind]];
-                }
-                dp[ind][t]=min(take,nottake);
-            }
-
-        }
-        if(dp[n-1][amount]>=1e9) return -1;
-        else return dp[n-1][amount];
+         int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int ans=findsum(coins,amount,n-1,dp);
+        if(ans>=1e9) return -1;
+        return ans;
     }
 };
