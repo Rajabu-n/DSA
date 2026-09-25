35689020fc31114ad7e4f2a90889ac81f385e402
Rajabu Nisha <rajabunisha1907@gmail.com>
2026-09-25 20:56:31 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (42.07%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -13,7 +13,8 @@ class Solution {
 public:
     bool check(TreeNode* left,TreeNode* right)
     {
-        if(left==NULL || right==NULL) return left==right;
+        if(left==NULL && right==NULL) return true;
+        if(left==NULL || right==NULL) return false;
         if(left->val!=right->val) return false;
         return check(left->left,right->right)&& check(left->right,right->left);
     }
